638d2fff4460bc579824f7a07d61478f2a05ba4b
doganmost <dogan@mostidea.com.tr>
2021-08-05 12:47:00 +0300

    static.js localhost edit

--- wwwroot/js/static.js
+++ wwwroot/js/static.js
@@ -1,6 +1,6 @@
-window.baseUrl = 'https://tssapi.sencard.com.tr'
+//window.baseUrl = 'https://tssapi.sencard.com.tr'
 // window.baseUrl = 'http://localhost:5000'
-window.returnUrl = 'https://market.sencard.com.tr'
+//window.returnUrl = 'https://market.sencard.com.tr'
 //window.returnUrl='https://localhost:5002'
 
 
